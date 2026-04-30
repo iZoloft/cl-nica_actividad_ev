c304ba4d6543fffc81191df5c35907fbf0549388
Ariel <akoppg@gmail.com>
2026-04-30 16:35:30 -0400

    Finalizado

--- clinica.py
+++ clinica.py
@@ -5,33 +5,34 @@ try:
     nombre = input("Ingrese su nombre \n")
     edad= int(input("Ingrese su edad \n"))
     quintil = int(input("Ingrese el quintil en el que se encuentra (1 a 5)\n"))
-    if edad > 0 and quintil >= 1 or quintil < 5:
-        if edad <= 25 and quintil == 1 or quintil == 2:
+    if edad > 0 and (quintil >= 1 and quintil < 5):
+        if edad <= 25 and (quintil == 1 or quintil == 2):
             descuento_plan = .18
-        elif edad <= 25 and quintil == 3 or quintil == 4:
+        elif edad <= 25 and (quintil == 3 or quintil == 4):
             descuento_plan = .12
-        elif edad >= 26 and edad <= 45 and quintil == 1 or quintil == 2:
+        elif (edad >= 26 and edad <= 45) and (quintil == 1 or quintil == 2):
             descuento_plan = .12
-        elif edad >= 26 and edad <= 45 and quintil == 3 or quintil == 4:
+        elif (edad >= 26 and edad <= 45) and (quintil == 3 or quintil == 4):
             descuento_plan = .08
         else:
             descuento_plan = 0
         
         if quintil == 1 or quintil == 2 or quintil == 3:
             descuento_radiografia = .10
-        else:
-            descuento_radiografia = 0
-        if edad > 40:
-            descuento_edad = .05
+            if edad >= 40:
+                descuento_edad = .05
+            else:
+                descuento_edad = 0
         else:
             descuento_edad = 0
+            descuento_radiografia = 0
         
-    total_plan = PLAN_DENTAL - (PLAN_DENTAL * descuento_plan)
-    total_radiografia = RADIOGRAFIA_DENTAL - (RADIOGRAFIA_DENTAL * descuento_radiografia) - (RADIOGRAFIA_DENTAL * descuento_edad)
-    print(f"Nombre: {nombre}")
-    print(f"Edad: {edad}")
-    print(f"Quintil: {quintil}")
-    print(f"Valor plan: {total_plan}")
-    print(f"Valor radiografia: {total_radiografia}")
+        total_plan = PLAN_DENTAL - (PLAN_DENTAL * descuento_plan)
+        total_radiografia = RADIOGRAFIA_DENTAL - (RADIOGRAFIA_DENTAL * descuento_radiografia) - (RADIOGRAFIA_DENTAL * descuento_edad)
+        print(f"Nombre: {nombre}")
+        print(f"Edad: {edad}")
+        print(f"Quintil: {quintil}")
+        print(f"Valor plan: {total_plan}")
+        print(f"Valor radiografia: {total_radiografia}")
 except:
     print("Valor debe ser numérico")
--- rango_num.py
+++ rango_num.py
@@ -4,54 +4,50 @@ num1 = int(input("Ingrese límite inferior\n"))
 num2 = int(input("Ingrese límite superior\n"))
 numero = randint(num1, num2)
 
-adivine = int(input("Adivina el número \n"))
+try:
 
-if numero % 2 != 0:
-    numero = numero +1
-if numero > num2:
-    numero = numero -1
-
-if adivine < num1 and adivine > num2:
-    print("Número esta fuera del rango, intente otra vez")
-
-if adivine == numero:
-    print("Has adivinado el número")
-else:
-    print("Aún no adivinas el número")
-
-if adivine > numero:
-    print("El número a adivinar es menor")
-else:
-    print("El número a adivinar es mayor")
-
-adivine2 = int(input("Intente otra vez\n"))
-
-if adivine2 < num1 and adivine > num2:
-    print("Número esta fuera del rango, intente otra vez")
-
-if adivine2 == numero:
-    print("Has adivinado el número")
-else:
-    print("Aún no adivinas el número")
-
-if adivine2 > numero:
-    print("El número a adivinar es menor")
-else:
-    print("El número a adivinar es mayor")
-
-cercania1 = numero - adivine
-cercania2 = numero - adivine2
-
-if cercania1 < cercania2:
-    print(f"El número esta mas cerca de {adivine} que de {adivine2}")
-else:
-    print(f"El número esta mas cerca de {adivine2} que de {adivine}")
-adivine3 = int(input("Intente una ultima vez\n"))
-
-if adivine3 < num1 and adivine > num2:
-    print("Número esta fuera del rango, intente otra vez")
-
-if adivine3 == numero:
-    print("Has adivinado el número")
-else:
-    print(f"No adivinaste el número, el número era {numero}")
+    if numero % 2 != 0:
+        numero = numero + 1
+        if numero > num2:
+            numero = numero - 2
+    adivina = int(input("Adivine el número \n"))
+    if adivina >= num1 and adivina <= num2:
+        if adivina == numero:
+            print("Adivinaste!")
+        else:
+            print("Intente otra vez")
+            if numero > adivina:
+                print("El número es mayor")
+            else:
+                print("El número es menor")
+            adivina2 = int(input("Intente denuevo\n"))
+            if adivina2 == numero:
+                print("Adivinaste")
+            else:
+                print("Intente otra vez")
+                if numero > adivina2:
+                    print("El número es mayor")
+                else:
+                    print("El número es menor")
+                if numero > adivina:
+                    distancia1 = numero - adivina
+                else:
+                    distancia1 = adivina - numero
+                if numero > adivina2:
+                    distancia2 = numero - adivina2
+                else:
+                    distancia2 = adivina2 - numero
+                
+                if distancia1 < distancia2:
+                    print(f"El número esta más cerca de {adivina}")
+                else:
+                    print(f"El número esta más cerca de {adivina2}")
+                adivina3 = int(input("Intente la última vez \n"))
+                if adivina3 == numero:
+                    print("Adivinaste")
+                else:
+                    print(f"El número era {numero}")
+    else:
+        print("el número adivinado debe estar dentro de los limites")
+except:
+    print("debe ser un número entero")
